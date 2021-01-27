046d04d29e767a8531586517191521c8e939b297
Yoshi3lines <ny3line@gmail.com>
2021-01-27 08:55:03 +0000

    topページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
-  get 'static_pages/top'
+  root 'static_pages#top'
+  get '/signup', to: 'users#new'
+  
+  # ログイン機能
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users do
+    collection { post :import }
+    member do
+      get 'edit_basic_info'
+      patch 'update_basic_info'
+      get 'attendances/edit_one_month'
+      patch 'attendances/update_one_month'
+    end
+    resources :attendances, only: :update
+  end
 end
