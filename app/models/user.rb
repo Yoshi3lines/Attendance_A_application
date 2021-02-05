a02bf64525aaa8ad669693672ef630c60698e07b
Yoshi3lines <ny3line@gmail.com>
2021-02-05 22:21:05 +0000

    userモデルの更新を許可するコメント部分を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,11 +19,19 @@ class User < ApplicationRecord
   # インポート機能
   def self.import(file)
     CSV.foreach(file.path, encoding: 'cp932:utf-8', headers: true) do |row|
+       # IDが見つかれば、レコードを呼び出し、見つかれなければ、新しく作成
       user = find_by(id: row["id"]) || new
+       # CSVからデータを取得し、設定する
       user.attributes = row.to_hash.slice(*updatable_attributes)
       user.save!(:validation => false)
     end
   end
+  # 更新を許可するカラムを定義
+  def self.updatable_attributes
+    ["name", "email", "affiliation", "employee_number", "uid", "basic_time",
+     "designated_work_start_time", "designated_work_end_time","superior","admin","password"]
+  end
+  
   
   # 渡された文字列のハッシュ値を返す
   def User.digest(string)
