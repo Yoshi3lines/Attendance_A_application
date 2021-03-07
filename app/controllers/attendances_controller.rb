50fa977218fb7aa4164b9ce3175ba59e866eddfe
Yoshi3lines <ny3line@gmail.com>
2021-03-07 08:55:56 +0000

    勤怠変更ページを編集中

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,5 +1,5 @@
 class AttendancesController < ApplicationController
-  before_action :set_user, only: [:edit_one_month, :update_one_month, :edit_overtime_notice]
+  before_action :set_user, only: [:edit_one_month, :update_one_month, :edit_overtime_notice, :edit_one_month_notice]
   before_action :logged_in_user, only: [:update, :edit_one_month]
   before_action :admin_or_correct_user, only: [:update, :edit_one_month, :update_one_month]
   before_action :set_one_month, only: :edit_one_month
@@ -7,7 +7,8 @@ class AttendancesController < ApplicationController
   before_action :correct_not, only: [:show, :edit_one_month]
   
   UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
-
+  
+  # 出勤、退勤の機能
   def update
     @user = User.find(params[:user_id])
     @attendance = Attendance.find(params[:id])
@@ -28,24 +29,134 @@ class AttendancesController < ApplicationController
     redirect_to @user
   end
   
+  
+  # ここからは勤怠変更申請
+  
+  # 勤怠変更申請
   def edit_one_month
+    @attendance = Attendance.find(params[:id])
+    @superior = User.where(sperior: true).where.not( id: current_user.id)
+  end
+  
+  # 勤怠変更申請お知らせモーダル
+  def edit_one_month_notice
+    @users = User.joins(:attendances).group("user_id").where(attendances: {indicater_reply_edit: "申請中"})
+    @attendances = Attendance.where.not(started_edit_at: nil, finished_edit_at: nil, note: nil, indicater_reply_edit: nil).order("worked_on ASC")
   end
   
   def update_one_month
     ActiveRecord::Base.transaction do # トランザクションを開始します。
-      attendances_params.each do |id, item|
-        attendance = Attendance.find(id)
-        attendance.update_attributes!(item)
+      c1 = 0 # カラムを更新した件数を入れる変数を定義
+      attendances_params.each do |id, item| # ストロングパラメータのidと各カラムを配列で回す処理
+        @attendance = Attendance.find(id) # attendancesテーブルから一つのidを探す
+          if item[:indicater_check_edit].present? # 上長が選択されていることを確認
+            if item[:started_edit_at].blank? && item[:finished_edit_at].present? #時間が入ってない場合はエラー
+              flash[:danger] = "出勤時刻が存在しません"
+              redirect_to attendances_edit_one_month_user_url(date: params[:date])
+              return
+            elsif item[:started_edit_at].present? && item[:finished_edit_at].blank? #出勤時間が入っているのに退勤時間が入ってない場合はエラー
+              flash[:danger] = "退勤時間が存在しません"
+              redirect_to attendances_edit_one_month_user_url(date: params[:date])
+              return
+            elsif item[:started_edit_at].blank? && item[:finished_edit_at].blank? # どちらの時間も入力されていない場合はエラー
+              flash[:danger] = "時刻を入力して下さい"
+              redirect_to attendances_edit_one_month_user_url(date: params[:date])
+              return
+              # 翌日チェックがなくて、さらに出勤時間よりも退勤時間が小さい場合はエラー
+            elsif item[:started_edit_at].present? && item[:finished_edit_at].present? && item[:tomorrow_edit] == "0" && item[:started_edit_at].to_s > item[:finished_edit_at].to_s
+              flash[:danger] = "入力時刻に誤りがあります"
+              redirect_to attendances_edit_one_month_user_url(date: params[:date])
+              return
+            elsif item[:note].blank? # 備考が空の場合はエラー
+              flash[:danger] = "変更内容を入力して下さい"
+              redirect_to attendances_edit_one_month_user_url(date: params[:date])
+              return
+            end
+            c1 += 1
+            attendance.update_attributes!(item)
+          end
+      end
+      if c1 > 0
+        flash[:success] = "勤怠変更を#{c1}件受け付けました"
+        redirect_to user_url(@user)
+      else
+        flash[:danger] = "上長を選択して下さい"
+        redirect_to attendances_edit_one_month_user_url(date: params[:date])
+        return
       end
     end
-    flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
-    redirect_to user_url(date: params[:date])
   rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
     flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
     redirect_to attendances_edit_one_month_user_url(date: params[:date])
+    return
+  end
+  # 翌日のチェックがあれば
+  # if item[:tomorrow] == "1" になり
+  #   item[:worked_on]で今日の日付を取得。また今日か明日の日付を取得し、これをtomorrow_dayに代入
+  #   tomorrow_day = @attendance.worked_on.tomorrow.to_s
+  #   退勤時刻のカラム = 上記で作成したtomorrow_dayを文字にしたもの + 空欄 + 退勤時間を文字にし、:00を渡すことで日本時間にしている
+  #   item[:finished_edit_at] = tomorrow_day.to_s + "　" + item[:finished_edit_at].to_s + ":00"
+  # else
+  #   退勤時刻のカラム = 今日の日付を文字にしたもの + 空欄 + 退勤時間を文字にして、:00を渡すことで日本時間にしている
+  #   item[:finished_edit_at] = @attendance.to_s + "　" + item[:finished_edit_at].to_s + ":00"
+  # end
+  # その後にカラムを更新させる
+  
+  # 勤怠変更申請お知らせモーダル更新
+  def update_one_month_notice
+    ActiveRecord::Base.transaction do
+      e1 = 0
+      e2 = 0
+      e3 = 0
+      attendances_notice_params.each do |id, item|
+        if item[:indicater_reply_edit].present?
+          if (item[:change_edit] == "1") && (item[:indicater_reply_edit] == "なし" || item[:indicater_reply_edit] == "承認" || item[:indicater_reply_edit] == "否認")
+            attendance = Attendance.find(id)
+            user = User.find(attendance.user_id)
+            if item[:indicater_reply_edit] == "なし"
+              e1 += 1
+              item[:started_edit_at] = nil
+              item[:finished_edit_at] = nil
+              item[:tomorrow] = nil
+              item[:note] = nil
+              item[:indicater_check_edit] = nil
+            elsif item[:indicater_reply_edit] == "承認"
+              if attendance.started_before_at.blank?
+                item[:started_before_at] = attendance.started_at
+              end
+              item[:started_at] = attendance.started_edit_at
+              if attendance.finished_before_at.blank?
+                item[:finished_before_at] = attendance.finished_at
+              end
+              item[:finished_at] = attendance.finished_edit_at
+              item[:indicater_check_edit] = nil
+              e2 += 1
+              attendance.indicater_check_anser = "勤怠変更申請を承認しました"
+            elsif item[:indicater_reply_edit] == "否認"
+              item[:started_edit_at] = nil
+              item[:finished_edit_at] = nil
+              item[:tomorrow] = nil
+              item[:note] = nil
+              item[:indicater_check_edit] = nil
+              e3 += 1
+              attendance.indicater_check_anser = "勤怠変更申請を否認しました"
+            end
+            attendance.update_attributes!(item)
+          end
+        else
+          flash[:danger] = "指示者確認を更新、または変更にチェックを入れて下さい"
+          redirect_to user_url(params[:user_id])
+          return
+        end
+      end
+    end
+  rescue ActiveRecord::RecordInvalid
+    flash[:danger] = "無効なデータがあった為、更新をキャンセルしました"
+    redirect_to edit_one_month_notice_user_attendance_url(@user, item)
   end
   
-  # ここから残業申請のモーダル
+   
+  # ここから通常の残業申請
   
   # 残業申請モーダル
   def edit_overtime_request
@@ -122,6 +233,10 @@ class AttendancesController < ApplicationController
       params.require(:user).permit(attendances: [:worked_on, :started_at, :finished_at, :started_edit_at, :finished_edit_at, :tomorrow_edit, :note, :indicater_check_edit, :indicater_reply_edit])[:attendances]
     end
     
+    # 勤怠編集のお知らせモーダル
+    def attendances_notice_params
+      params.require(:user).permit(attendances: [:started_at, :finished_at, :started_before_at, :finished_before_at, :started_edit_at, :finished_edit_at, :note, :indicater_reply_edit, :change_edit])[:attendances]
+    end
     
     # 残業申請モーダル情報
     def overtime_params
@@ -134,7 +249,17 @@ class AttendancesController < ApplicationController
       params.require(:user).permit(attendances: [:overtime_work, :indicater_reply, :change, :indicater_check, :overtime_finished_at, :indicater_check_anser])[:attendances]
     end
     
-    # 1か月の承認モーダルのparamsは未設定
+    # 1ヶ月承認申請
+    def month_approval_params
+      # attendanceテーブルの（承認月、指示者確認、どの上長なのか？）
+      params.require(:user).permit(:month_approval, :indicater_reply_month, :indicater_check_month)
+    end
+    
+    # 1ヶ月承認申請お知らせモーダル
+    def month_approval_notice_params
+      # attendancesテーブルの（承認月、指示者確認、変更、どの上長なのか？）
+      params.require(:user).permit(attendances: [:month_approval, :indicater_reply_month, :change_month, :indicater_check_month])[:attendances]
+    end
     
     # beforeフィルター
     
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -9,17 +9,17 @@ class Attendance < ApplicationRecord
   enum indicater_reply_month: { "なし" => 0, "承認" => 1, "否認" => 2, "申請中" => 4 }, _prefix: true
   
   # 出勤時間が存在しない場合の退勤時間は無効
-  validate :finished_at_is_invalid_without_a_started_at
+  # validate :finished_at_is_invalid_without_a_started_at
   # 出勤、退勤時間が共に存在する場合、出勤時間より早い退勤時間は無効
-  validate :started_at_than_finished_at_fast_if_invalid
+  # validate :started_at_than_finished_at_fast_if_invalid
   
-  def finished_at_is_invalid_without_a_started_at
-    errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
-  end
+  # def finished_at_is_invalid_without_a_started_at
+  #   errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
+  # end
   
-  def started_at_than_finished_at_fast_if_invalid
-    if started_at.present? && finished_at.present?
-      errors.add(:started_at, "より早い退勤時間は無効です") if started_at > finished_at
-    end
-  end
+  # def started_at_than_finished_at_fast_if_invalid
+  #   if started_at.present? && finished_at.present?
+  #     errors.add(:started_at, "より早い退勤時間は無効です") if started_at > finished_at
+  #   end
+  # end
 end
