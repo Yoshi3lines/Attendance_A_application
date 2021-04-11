0ae406c8f9aa8438ebced65008c90310ca8f8cd3
Yoshi3lines <ny3line@gmail.com>
2021-04-11 05:06:32 +0000

    完成vol.2

--- db/schema.rb
+++ db/schema.rb
@@ -65,14 +65,14 @@ ActiveRecord::Schema.define(version: 20210327021006) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "affiliation"
-    t.datetime "basic_time", default: "2021-04-09 23:00:00"
-    t.datetime "work_time", default: "2021-04-09 22:30:00"
+    t.datetime "basic_time", default: "2021-04-10 23:00:00"
+    t.datetime "work_time", default: "2021-04-10 22:30:00"
     t.time "designated_work_start_time", default: "2000-01-01 00:00:00"
     t.time "designated_work_end_time", default: "2000-01-01 09:00:00"
     t.string "employee_number"
     t.string "uid"
     t.boolean "superior", default: false
-    t.datetime "basic_work_time", default: "2021-04-09 23:00:00"
+    t.datetime "basic_work_time", default: "2021-04-10 23:00:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
